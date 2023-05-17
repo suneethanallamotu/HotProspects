344a1f9bd254a67791e281fa2d02ad0b0fdb8c0f
Suneetha Nallamotu <suneetha_n@hotmail.com>
2023-05-16 22:19:21 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Suneetha Nallamotu on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
